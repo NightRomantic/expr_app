84afc535015a03271373916a7653a936091c6338
DESKTOP-1RVG80B\evgen <nightromantic63@gmail.com>
2021-04-04 20:05:21 +0400

    fetching posts from db

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,15 @@ const router = express.Router();
 module.exports = ({ db }) => {
 
   router.get('/', (req, res) => {
-    res.render('index', { title: 'QEQ' });
+    db.posts.find({}, function (err, docs) {
+      if (err) throw new Error(err);
+  
+      res.render('index', {
+        title: 'Posts list',
+        date: Date.now(),
+        posts: docs,
+      });
+    });
   });
 
   return router;
